a962c5f923235837e3333f4b5c4918b3c084fee9
fredbern <fre19036@byui.edu>
2022-09-15 21:47:15 +1100

    c

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const links = [
+    {
+        label: "Week1 notes",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week2 notes",
+        url: "week2/index.html"
+    }
+
+]
+
+function loadIndex() {
+    const ol = document.querySelector("#linksList");
+
+    links.forEach(link => {
+        const li = document.createElement("li");
+        const href = document.createElement("a");
+        href.setAttribute("href", link.url);
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(li);
+    })
+
+}

d3c56ff469e9b34c384981ea0b19397ad5e6f372
fredbern <fre19036@byui.edu>
2022-11-16 01:22:26 +1100

    c

--- week8/groupwork/scripts/Model.js
+++ week8/groupwork/scripts/Model.js
@@ -1,5 +1,5 @@
 // normally the model would have more going on...retrieving the hikes from a database, filtering, etc. Our model will be very simple.  We could simply export the hikeList, but a better pattern would be to create a 'getter' function to do it instead. That way as our model changed...we could simply change the getter function and anything using it should be able to remain the same.
-const Url = `https://swapi.dev/api/people/`;
+const Url = `https://swapi.py4e.com/api/people` //`https://swapi.dev/api/people/`;
 
 export default class Model {
   Link(Link) {
--- week8/groupwork/scripts/app.js
+++ week8/groupwork/scripts/app.js
@@ -1,7 +1,7 @@
 import List from './Controller.js';
 //on load grab the array and insert it into the page
 const myList = new List('list');
-let Link = `https://swapi.dev/api/people/`;
+let Link = `https://swapi.py4e.com/api/people`;
 window.addEventListener('load', () => {
   myList.GetLink(Link);
 });

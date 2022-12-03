e43b33f0a784f2949f96a94d6e599aec7b3c7f75
fredbern <fre19036@byui.edu>
2022-12-04 07:46:22 +1100

    c

--- js/main.js
+++ js/main.js
@@ -34,6 +34,18 @@ const links = [
     {
         label: "Week9 notes",
         url: "week9/index.html"
+    },
+    {
+        label: "Week10 notes",
+        url: "week10/index.html"
+    },
+    {
+        label: "Week11 notes",
+        url: "week11/index.html"
+    },
+    {
+        label: "Week12 notes",
+        url: "week12/index.html"
     }
 ]
 
